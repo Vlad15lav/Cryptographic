f61febc27b00838b84ea6cdb5aba61858ef7c948
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-02-25 20:05:35 +0300

    Update utils.py

--- Williams p+1/tools/utils.py
+++ Williams p+1/tools/utils.py
@@ -1,4 +1,14 @@
+# Find GCD
+def find_gcd(a: int, b: int) -> int:
+    while b:
+        a, b = b, a % b
+    return a
+
 # Sequence Luke
-def luke_sequence(u: list, P: int = 1, Q: int = -1) -> list:
-    u.append(u[-1] * P + u[-2] * Q)
-    return u
+def luke_sequence(last_u: int, cur_u: int, count: int, P: int = 1, Q: int = -1) -> int:
+    temp_u = 0
+    for _ in range(count):
+        temp_u = last_u * P + cur_u * Q
+        last_u = cur_u
+        cur_u = temp_u
+    return temp_u
