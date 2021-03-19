741dadd51b5e27bd453637e67f6ecadec699ce3f
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-03-19 12:51:26 +0300

    Update prime_test.py

--- Prime Test/tools/prime_test.py
+++ Prime Test/tools/prime_test.py
@@ -1,32 +1,42 @@
+import math
 import random
 from typing import Callable
 from tools.tool import find_gcd, legendre
 
 # Test Miller Rabin
 def miller_rabin(n: int) -> bool:
-    if n <= 1 or n == 4: return False
-    if n <= 3: return True
+    if n == 2: return True
+    if n % 2 == 0: return False
+
     d = n - 1
     # find d: n - 1 = 2^t * d
     while not d % 2: d //= 2
 
-    a = random.randint(2, n - 1)
-    # first step
-    x = pow(a, d, n)
-    if x == 1 or x == n - 1: return True
-    # second step
-    while d != n - 1:
-        x, d = pow(x, 2, n), d * 2
-        if x == 1: return False
-        if x == n - 1: return True
-    return False
+    for _ in range(int(math.log2(n))):
+        a = random.randint(2, n - 1)
+        # first step
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        # second step
+        while d != n - 1:
+            x, d = pow(x, 2, n), d * 2
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
+# Test Solovay Strassen
 def solovay_strassen(n: int) -> bool:
-    if n <= 1 or n == 4: return False
-    if n <= 3: return True
+    if n == 2: return True
+    if n % 2 == 0: return False
 
-    a = random.randint(2, n - 1)
-    d = find_gcd(a, n)
+    for _ in range(int(math.log2(n))):
+        a = random.randint(2, n - 1)
+        d = find_gcd(a, n)
 
-    if d > 1: return False
-    return pow(a, (n - 1) // 2, n)  == legendre(a, n) % n
+        if d > 1: return False
+        if pow(a, (n - 1) // 2, n) != legendre(a, n) % n:
+            return False
+    return True
