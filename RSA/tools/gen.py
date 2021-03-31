597a9c24a4142f19b59df6f977afbeb3a574bca0
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2021-03-31 20:35:21 +0300

    Update gen.py

--- RSA/tools/gen.py
+++ RSA/tools/gen.py
@@ -3,20 +3,19 @@ from tools.tool import find_gcd
 from tools.prime_test import miller_rabin
 
 # generate number where size is binary length
-def generate_number(size: int) -> int:
-    return random.randint(pow(2, size - 1), pow(2, size) - 1)
+def generate_number(length: int) -> int:
+    return random.randint(pow(2, length - 1), pow(2, length) - 1)
 
 # generate prime number
-def generate_prime(size: int) -> int:
-    n = generate_number(size)
+def generate_prime(length: int) -> int:
+    n = generate_number(length)
     if n == 2: return n
     n += n % 2 == 0 # to odd
 
-    while not miller_rabin(n):
-        n += 2
+    while not miller_rabin(n): n += 2
     return n
 
-def generate_public(N: int, euler: int) -> int:
-    e = N // 3
+def generate_public(length: int, euler: int) -> int:
+    e = generate_number(length)
     while find_gcd(e, euler) != 1: e += 1
     return e
